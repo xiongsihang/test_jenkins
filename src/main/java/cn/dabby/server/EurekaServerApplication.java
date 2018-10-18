d92e86ce45cf77bac999c419acf03286be022a06
xiongsihang <xiongsihang@DESKTOP-L56FIRJ>
2018-10-18 15:25:07 +0800

    添加接口

--- src/main/java/cn/dabby/server/EurekaServerApplication.java
+++ src/main/java/cn/dabby/server/EurekaServerApplication.java
@@ -3,12 +3,20 @@ package cn.dabby.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @EnableEurekaServer
 @SpringBootApplication
+@RestController
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+	
+	@RequestMapping("/getname")
+	public String getName() {
+		return "I am Bob";
 	}
 }
